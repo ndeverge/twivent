27ae034c563416363d21f6783632b4426b79badc
Nicolas Deverge <ndeverge@ekito.fr>
2013-01-26 15:12:17 +0100

    First import

---
+++ app/Global.scala
@@ -0,0 +1,26 @@
+import play.api.GlobalSettings
+import play.api.Logger
+import play.api.Application
+import play.libs.Akka
+import scala.concurrent.duration._
+import akka.actor.Props
+import akka.actor.Actor
+import play.api.libs.concurrent.Execution.Implicits._
+import java.text.SimpleDateFormat
+import java.util.Date
+
+object Global extends GlobalSettings {
+
+  override def onStart(app: Application) {
+
+    val Tick = "tick"
+    val tickActor = Akka.system.actorOf(Props(new Actor {
+      def receive = {
+        case Tick => Logger.info("tock at " + new SimpleDateFormat("dd-MM-yyyy HH:mm:ss").format(new Date()))
+      }
+    }))
+
+    Akka.system.scheduler.schedule(0 seconds, 10 seconds, tickActor, "tick")
+  }
+
+}
