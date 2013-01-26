18470b2de4e8fcf23164d42f1eae1eac988e175b
Nicolas Deverge <ndeverge@ekito.fr>
2013-01-26 17:54:42 +0100

    Self ping

--- app/Global.scala
+++ app/Global.scala
@@ -8,6 +8,7 @@ import akka.actor.Actor
 import play.api.libs.concurrent.Execution.Implicits._
 import java.text.SimpleDateFormat
 import java.util.Date
+import play.api.libs.ws.WS
 
 object Global extends GlobalSettings {
 
@@ -16,11 +17,14 @@ object Global extends GlobalSettings {
     val Tick = "tick"
     val tickActor = Akka.system.actorOf(Props(new Actor {
       def receive = {
-        case Tick => Logger.info("tock at " + new SimpleDateFormat("dd-MM-yyyy HH:mm:ss").format(new Date()))
+        case Tick => {
+          Logger.info("tock at " + new SimpleDateFormat("dd-MM-yyyy HH:mm:ss").format(new Date()))
+          WS.url("http://twivent.herokuapp.com").get.map(response => println("ping status: " + response.status))
+        }
       }
     }))
 
-    Akka.system.scheduler.schedule(0 seconds, 10 seconds, tickActor, "tick")
+    Akka.system.scheduler.schedule(0 seconds, 10 minutes, tickActor, "tick")
   }
 
 }
