748bc87a8b24cf31a9f0993fe998f7ae8ca49600
Nicolas Deverge <ndeverge@ekito.fr>
2013-02-24 14:06:18 +0100

    Mark an Event as already notified when tweeting

--- app/actor/ReminderActor.scala
+++ app/actor/ReminderActor.scala
@@ -13,7 +13,11 @@ class ReminderActor extends Actor {
       Logger.debug("Looking for events to notify...")
       val nextIncomingEvents = GoogleCalendar.nextIncomingEvents()
       Reminder.filterEventsToNotify(nextIncomingEvents).foreach {
-        eventToNotify => Twitter.sendTweet(eventToNotify).map(tweet => Logger.info("Tweeting " + tweet.getText()))
+        eventToNotify =>
+          {
+            Twitter.sendTweet(eventToNotify).map(tweet => Logger.info("Tweeting " + tweet.getText()))
+            GoogleCalendar.markAsNotified(eventToNotify.id)
+          }
       }
     }
   }
